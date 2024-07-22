65a06b656011ea3a6452968575742fb7859b9f62
Lapa150 <ig604057@gmail.com>
2024-07-22 13:13:11 -0300

    Actualizacion de prueba

--- src/main/java/com/example/demo/TpApplication.java
+++ src/main/java/com/example/demo/TpApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-
+//Mensaje para ver si funciona la actualizacion
 public class TpApplication {
 
 	public static void main(String[] args) {
